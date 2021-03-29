12475c4c173d3563fb2f498416357d7df3a2314f
wangzhancheng <1748995976@qq.com>
2021-03-29 16:42:57 +0800

    完成我的订单页面整体设计

--- app/src/main/java/com/wzc1748995976/hotelbooking/ui/order/OrderFragment.kt
+++ app/src/main/java/com/wzc1748995976/hotelbooking/ui/order/OrderFragment.kt
@@ -6,13 +6,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.drakeet.multitype.MultiTypeAdapter
 import com.wzc1748995976.hotelbooking.R
+import com.wzc1748995976.hotelbooking.ui.anotherAdapter.FacilityInfo
+import com.wzc1748995976.hotelbooking.ui.anotherAdapter.FacilityInfoDelegate
 
 class OrderFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = OrderFragment()
-    }
 
     private lateinit var viewModel: OrderViewModel
 
@@ -26,7 +27,24 @@ class OrderFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(OrderViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        val recyclerView = view?.findViewById<RecyclerView>(R.id.recyclerView)
+
+        val orderItemAdapter = MultiTypeAdapter()
+        val orderItemAdapterItems = ArrayList<Any>()
+        recyclerView?.visibility = View.VISIBLE
+        recyclerView?.layoutManager = LinearLayoutManager(activity)
+        orderItemAdapter.register(FinishUseOrderInfoDelegate())
+        orderItemAdapter.register(WaitEvaOrderInfoDelegate())
+        orderItemAdapter.register(BookSuccessOrderInfoDelegate())
+        recyclerView?.adapter = orderItemAdapter
+        for (i in 0..1){
+            orderItemAdapterItems.add(FinishUseOrderInfo("影音体验"))
+            orderItemAdapterItems.add(WaitEvaOrderInfo("影音体验"))
+            orderItemAdapterItems.add(BookSuccessOrderInfo("影音体验"))
+        }
+        orderItemAdapter.items = orderItemAdapterItems
+        orderItemAdapter.notifyDataSetChanged()
     }
 
 }
--- app/src/main/java/com/wzc1748995976/hotelbooking/ui/order/OrderRecyclerAdapter.kt
+++ app/src/main/java/com/wzc1748995976/hotelbooking/ui/order/OrderRecyclerAdapter.kt
@@ -1,2 +1,117 @@
 package com.wzc1748995976.hotelbooking.ui.order
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.drakeet.multitype.ItemViewDelegate
+import com.wzc1748995976.hotelbooking.HotelBookingApplication
+import com.wzc1748995976.hotelbooking.R
+import com.wzc1748995976.hotelbooking.ui.livedcollection.HotelInfo
+
+data class FinishUseOrderInfo(
+    val name: String//酒店名称
+)
+data class WaitEvaOrderInfo(
+    val name: String//酒店名称
+)
+data class BookSuccessOrderInfo(
+    val name: String//酒店名称
+)
+
+class FinishUseOrderInfoDelegate: ItemViewDelegate<FinishUseOrderInfo, FinishUseOrderInfoDelegate.ViewHolder>() {
+
+    interface ClickOrderItem{
+        fun getResultToSet(holder: ViewHolder, item: FinishUseOrderInfo)
+    }
+
+    private var clickOrderItem: ClickOrderItem? = null
+    fun setClickOrderItem(newObject: ClickOrderItem){
+        clickOrderItem = newObject
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val nameView: TextView = itemView.findViewById(R.id.hotelName)
+    }
+
+    override fun onCreateViewHolder(context: Context, parent: ViewGroup): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.order_item_finish_use, parent, false)
+        return ViewHolder(view)
+    }
+
+
+    override fun onBindViewHolder(holder: ViewHolder, item: FinishUseOrderInfo) {
+        holder.run {
+            nameView.text = item.name
+        }
+        holder.itemView.setOnClickListener {
+            clickOrderItem?.getResultToSet(holder, item)
+        }
+    }
+}
+
+class WaitEvaOrderInfoDelegate: ItemViewDelegate<WaitEvaOrderInfo, WaitEvaOrderInfoDelegate.ViewHolder>() {
+
+    interface ClickOrderItem{
+        fun getResultToSet(holder: ViewHolder, item: WaitEvaOrderInfo)
+    }
+
+    private var clickOrderItem: ClickOrderItem? = null
+    fun setClickOrderItem(newObject: ClickOrderItem){
+        clickOrderItem = newObject
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val nameView: TextView = itemView.findViewById(R.id.hotelName)
+    }
+
+    override fun onCreateViewHolder(context: Context, parent: ViewGroup): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.order_item_wait_eva, parent, false)
+        return ViewHolder(view)
+    }
+
+
+    override fun onBindViewHolder(holder: ViewHolder, item: WaitEvaOrderInfo) {
+        holder.run {
+            nameView.text = item.name
+        }
+        holder.itemView.setOnClickListener {
+            clickOrderItem?.getResultToSet(holder, item)
+        }
+    }
+}
+
+class BookSuccessOrderInfoDelegate: ItemViewDelegate<BookSuccessOrderInfo, BookSuccessOrderInfoDelegate.ViewHolder>() {
+
+    interface ClickOrderItem{
+        fun getResultToSet(holder: ViewHolder, item: BookSuccessOrderInfo)
+    }
+
+    private var clickOrderItem: ClickOrderItem? = null
+    fun setClickOrderItem(newObject: ClickOrderItem){
+        clickOrderItem = newObject
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val nameView: TextView = itemView.findViewById(R.id.hotelName)
+    }
+
+    override fun onCreateViewHolder(context: Context, parent: ViewGroup): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.order_item_book_success, parent, false)
+        return ViewHolder(view)
+    }
+
+
+    override fun onBindViewHolder(holder: ViewHolder, item: BookSuccessOrderInfo) {
+        holder.run {
+            nameView.text = item.name
+        }
+        holder.itemView.setOnClickListener {
+            clickOrderItem?.getResultToSet(holder, item)
+        }
+    }
+}
