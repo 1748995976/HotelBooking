299c19cad99b1f27cd69e110e2dfd5a92e651bf1
wangzhancheng <1748995976@qq.com>
2021-04-20 12:01:03 +0800

    个人信息界面展示完成，下一步实现提交订单评价功能

--- app/src/main/java/com/wzc1748995976/hotelbooking/ui/mine/MineFragment.kt
+++ app/src/main/java/com/wzc1748995976/hotelbooking/ui/mine/MineFragment.kt
@@ -8,42 +8,73 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.lifecycle.Observer
 import com.bumptech.glide.Glide
 import com.bumptech.glide.Priority
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.wzc1748995976.hotelbooking.HotelBookingApplication
+import com.wzc1748995976.hotelbooking.MakePerfect.MyLinearLayout
 import com.wzc1748995976.hotelbooking.R
+import com.wzc1748995976.hotelbooking.logic.network.MyServiceCreator
 import jp.wasabeef.glide.transformations.BlurTransformation
 import jp.wasabeef.glide.transformations.CropCircleTransformation
+import kotlinx.android.synthetic.main.mine_item.view.*
 
 class MineFragment : Fragment() {
 
     private lateinit var viewModel: MineViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MineViewModel::class.java)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.mine_fragment, container, false)
-        //实现个人中心头部磨砂布局
-        val blurImageView = view.findViewById<ImageView>(R.id.iv_blur);
-        val avatarImageView = view.findViewById<ImageView>(R.id.iv_avatar);
-        Glide.with(activity)
-            .load("https://p0.meituan.net/movie/48774506dc0e68805bc25d2cd087d1024316392.jpg")
-            .bitmapTransform(BlurTransformation(activity, 25), CenterCrop(activity))
-            .priority(Priority.HIGH)
-            .into(blurImageView)
-        Glide.with(activity)
-            .load("https://p0.meituan.net/movie/48774506dc0e68805bc25d2cd087d1024316392.jpg")
-            .bitmapTransform(CropCircleTransformation(activity))
-            .priority(Priority.IMMEDIATE)
-            .into(avatarImageView)
+
+        viewModel.getUserInfo(HotelBookingApplication.account ?: "未知account")
+
+        val blurImageView = view.findViewById<ImageView>(R.id.iv_blur)
+        val avatarImageView = view.findViewById<ImageView>(R.id.iv_avatar)
+
+        val accountItem = view.findViewById<MyLinearLayout>(R.id.accountItem)
+        val nickNameItem = view.findViewById<MyLinearLayout>(R.id.nickNameItem)
+        val sexItem = view.findViewById<MyLinearLayout>(R.id.sexItem)
+        val ageItem = view.findViewById<MyLinearLayout>(R.id.ageItem)
+        val phoneItem = view.findViewById<MyLinearLayout>(R.id.phoneItem)
+        val locationItem = view.findViewById<MyLinearLayout>(R.id.locationItem)
+        val changeItem = view.findViewById<MyLinearLayout>(R.id.changeItem)
+        val aboutUsItem = view.findViewById<MyLinearLayout>(R.id.aboutUsItem)
+
+        viewModel.userInfoResult.observe(viewLifecycleOwner, Observer { result->
+            val data = result.getOrNull()
+            if(data != null){
+                accountItem.item_text_right.text = data.account
+                nickNameItem.item_text_right.text = data.name
+                sexItem.item_text_right.text = data.sex
+                ageItem.item_text_right.text = data.age
+                phoneItem.item_text_right.text = data.phone
+                locationItem.item_text_right.text = data.location
+                //实现个人中心头部磨砂布局
+                Glide.with(activity)
+                    .load(MyServiceCreator.userAvatar + data.avatar)
+                    .bitmapTransform(BlurTransformation(activity, 25), CenterCrop(activity))
+                    .priority(Priority.HIGH)
+                    .into(blurImageView)
+                Glide.with(activity)
+                    .load(MyServiceCreator.userAvatar + data.avatar)
+                    .bitmapTransform(CropCircleTransformation(activity))
+                    .priority(Priority.IMMEDIATE)
+                    .into(avatarImageView)
+            }
+        })
+
+
         return view
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MineViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
 
 }
--- app/src/main/java/com/wzc1748995976/hotelbooking/ui/mine/MineViewModel.kt
+++ app/src/main/java/com/wzc1748995976/hotelbooking/ui/mine/MineViewModel.kt
@@ -1,7 +1,17 @@
 package com.wzc1748995976.hotelbooking.ui.mine
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import com.wzc1748995976.hotelbooking.logic.Repository
 
 class MineViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    //获得用户的个人信息
+    private val userInfoLiveData = MutableLiveData<String>()
+    val userInfoResult = Transformations.switchMap(userInfoLiveData){
+        Repository.getInfoByAccount(userInfoLiveData.value ?: "未知account")
+    }
+    fun getUserInfo(account: String){
+        userInfoLiveData.value = account
+    }
 }
